94899e06c6ee6ee110eefaa8ac7c142861343e46
jonsoku2 <whdtjr2792@gmail.com>
2020-11-18 23:41:47 +0900

    테스트에서 enzyme 사용해보기

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,20 @@
 import React from "react";
-import Enzyme from "enzyme";
+import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
 import App from "./App";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 test("renders learn react link", () => {
-  //
+  const wrapper = shallow(<App />);
+  expect(wrapper).toBeTruthy();
 });
+
+// 1. console로 해당 돔을 보여준다.
+// console.log(wrapper.debug());
+
+// 2. toBeTruthy : true를 판단
+// expect(wrapper).toBeTruthy();
+
+// 2. toBeFalsy : false를 판단
+// expect(wrapper).toBeFalsy();

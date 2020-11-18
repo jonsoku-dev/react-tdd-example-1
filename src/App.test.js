54780a7b3479c4a434b2314056e4786caf64c4b3
jonsoku2 <whdtjr2792@gmail.com>
2020-11-19 00:15:25 +0900

    추가적인 data-test 속성 추가

--- src/App.test.js
+++ src/App.test.js
@@ -11,10 +11,22 @@ test("renders without error", () => {
   expect(appComponent.length).toBe(1);
 });
 
-test("renders button", () => {});
+test("renders button", () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='increment-button']");
+  expect(appComponent.length).toBe(1);
+});
 
-test("renders counter display", () => {});
+test("renders counter display", () => {
+  const wrapper = shallow(<App />);
+  const button = wrapper.find("[data-test='counter-display']");
+  expect(button.length).toBe(1);
+});
 
-test("counter starts at 0", () => {});
+test("counter starts at 0", () => {
+  const wrapper = shallow(<App />);
+  const counterDisplay = wrapper.find("[data-test='counter-display']");
+  expect(counterDisplay.length).toBe(1);
+});
 
 test("clicking on button increments counter display without error", () => {});
